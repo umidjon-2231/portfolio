9d030db7a91ad0e654a4b9586f9f625fd2d82980
umidjon-2231 <tumidjon808@gmail.com>
2025-02-04 23:59:44 +0500

    optimize Avatar.tsx

--- src/components/UI/Avatar.tsx
+++ src/components/UI/Avatar.tsx
@@ -1,5 +1,4 @@
-import React, {FC, useEffect, useState} from 'react';
-import Skeleton from "react-loading-skeleton";
+import React, {FC} from 'react';
 import Image from "next/image";
 
 interface AvatarProps {
@@ -10,27 +9,26 @@ interface AvatarProps {
 const AVATAR_URL = "/api/avatar"
 
 const Avatar: FC<AvatarProps> = ({src, image_size: IMAGE_SIZE}) => {
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
+    //
+    // useEffect(() => {
+    //     setLoading(true);
+    // }, [src, IMAGE_SIZE])
 
-    useEffect(() => {
-        setLoading(true);
-    }, [src, IMAGE_SIZE])
-
-    console.count("Avatar render")
+    // console.count("Avatar render")
 
     return (
         <div
-            className={" md:me-3 border-4 border-solid box-content border-[var(--secondary)] rounded-full"}
+            className={" md:me-3 border-4 border-solid box-content relative border-[var(--secondary)] rounded-full"}
             style={{width: IMAGE_SIZE, height: IMAGE_SIZE,}}>
-            {loading &&
-                <Skeleton borderRadius={999} width={IMAGE_SIZE} height={IMAGE_SIZE}
-                          style={{lineHeight: "inherit"}}/>
-            }
+            {/*{loading &&*/}
+            {/*    <Skeleton borderRadius={999} width={IMAGE_SIZE} height={IMAGE_SIZE}*/}
+            {/*              style={{lineHeight: "inherit", position: "absolute", inset: 0, zIndex: 10}}/>*/}
+            {/*}*/}
             <Image
-                loading={"eager"}
                 className={"rounded-full"}
-                hidden={loading}
-                onLoad={() => setLoading(false)}
+                // hidden={loading}
+                // onLoad={() => setLoading(false)}
                 src={src ?? AVATAR_URL}
                 alt={"my photo"}
                 width={IMAGE_SIZE} height={IMAGE_SIZE} placeholder={"empty"}
