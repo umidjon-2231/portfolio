4854b52e1704fa6138ec8a850244993382b1b6fa
umidjon-2231 <tumidjon808@gmail.com>
2025-04-05 18:08:39 +0500

    add google analytcs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,67 @@
-import type {Metadata} from "next";
-import './styles/chat.css';
+import type { Metadata } from "next";
+import "./styles/chat.css";
 import "./styles/globals.css";
 import React from "react";
-import {Inria_Sans} from "next/font/google"
-import {StoreProvider} from "@/components/StoreProvider";
-import 'react-loading-skeleton/dist/skeleton.css'
-import {SkeletonTheme} from "react-loading-skeleton";
+import { Inria_Sans } from "next/font/google";
+import { StoreProvider } from "@/components/StoreProvider";
+import "react-loading-skeleton/dist/skeleton.css";
+import { SkeletonTheme } from "react-loading-skeleton";
 import Chat from "@/components/Chat";
 import NoPrerender from "@/components/NoPrerender";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
     title: "Umid's portfolio",
-    description: "Portfolio of Umidjon Tojiboyev. Designed and coded by himself!",
-    keywords: ["Umid", "Umidjon", "Tojiboyev", "Umidjon Tojiboyev",
-        "portfolio", "web developer", "react", "typescript", "next.js", "tashkent", "uzbekistan"],
+    description:
+        "Portfolio of Umidjon Tojiboyev. Designed and coded by himself!",
+    keywords: [
+        "Umid",
+        "Umidjon",
+        "Tojiboyev",
+        "Umidjon Tojiboyev",
+        "portfolio",
+        "web developer",
+        "react",
+        "typescript",
+        "next.js",
+        "tashkent",
+        "uzbekistan",
+    ],
     creator: "Umidjon Tojiboyev",
     alternates: {
         canonical: "https://tojiboyevumidjon.uz/",
         languages: {
-            'en': 'https://tojiboyevumidjon.uz/en',
-            'ru': 'https://tojiboyevumidjon.uz/ru',
+            en: "https://tojiboyevumidjon.uz/en",
+            ru: "https://tojiboyevumidjon.uz/ru",
         },
-    }
+    },
 };
 
-const inria = Inria_Sans({weight: ["300", "400", "700"], subsets: ["latin"]})
+const inria = Inria_Sans({ weight: ["300", "400", "700"], subsets: ["latin"] });
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
+    const gaId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!;
+    
     return (
         <StoreProvider>
             <html lang={"en"}>
-            <body
-                className={inria.className}
-            >
-            {/*<Loading>*/}
-            <SkeletonTheme baseColor="var(--skeleton-base)" highlightColor="var(--skeleton-highlight)">
-                {children}
-            </SkeletonTheme>
-            <Chat/>
-            <NoPrerender/>
-            {/*</Loading>*/}
-            </body>
+                <body className={inria.className}>
+                    {/*<Loading>*/}
+                    <SkeletonTheme
+                        baseColor="var(--skeleton-base)"
+                        highlightColor="var(--skeleton-highlight)"
+                    >
+                        {children}
+                    </SkeletonTheme>
+                    <Chat />
+                    <NoPrerender />
+                    {/*</Loading>*/}
+                </body>
+                {gaId && <GoogleAnalytics gaId={gaId} />}
             </html>
         </StoreProvider>
     );
